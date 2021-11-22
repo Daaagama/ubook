7b151cf7aaf435cd731575522a0bcaf6b1fdc893
Andrew <drewg3r@gmail.com>
2021-11-22 11:43:06 +0200

    App base setup + lab6

---
+++ src/tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture
+def app():
+    """Create and configure a new app instance for each test."""
+    # create the app with common test config
+    app = create_app({"TESTING": True, "SECRET_KEY": "secret-key-for-tests"})
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    """A test client for the app."""
+    return app.test_client()
